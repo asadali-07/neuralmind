16f04e8061af2432fe772b170b8594360b414a4b
Asad Ali <mo.asadali007@gmail.com>
2025-08-24 14:37:01 +0530

    Add frontend on the server using public folder

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import { createSocketServer } from './socket/socket.js';
 import { connectDB } from './config/db.js';
 import  http from 'http';
 import cors from 'cors';
+import path from "path";
 
 configDotenv();
 
@@ -24,6 +25,7 @@ app.use(cors({
 }));
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.static(path.join(process.cwd(), "public")));
 
 
 
@@ -31,7 +33,9 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
 
-
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "public", "index.html"));
+});
 
 server.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
